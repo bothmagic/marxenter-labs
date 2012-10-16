46cbd45227da33390c0b8336b5b4154a002e6454
markus.taubert <markus.taubert@c8aec436-628a-29fb-4c76-4f2700b0ca23>
2012-10-16 10:09:07 +0000

    tagging qml graph prototyp 1

---
+++ tags/qml_graph_proto1/edgelayer.h
@@ -0,0 +1,44 @@
+#ifndef EDGELAYER_H
+#define EDGELAYER_H
+
+#include <QDeclarativeItem>
+
+class EdgeLayer : public QDeclarativeItem
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QDeclarativeItem* start READ start WRITE setStart)
+    Q_PROPERTY(QDeclarativeItem* end READ end WRITE setEnd)
+
+public:
+    explicit EdgeLayer(QDeclarativeItem *parent = 0);
+    void paint(QPainter *p, const QStyleOptionGraphicsItem *option, QWidget *widget = 0);
+
+
+
+    void setStart(QDeclarativeItem* start);
+    QDeclarativeItem* start() const;
+
+    void setEnd(QDeclarativeItem* end);
+    QDeclarativeItem* end() const;
+
+
+
+
+
+signals:
+    
+public slots:
+    void upaint();
+protected:
+    bool sceneEventFilter(QGraphicsItem *watched, QEvent *event);
+    bool eventFilter(QObject *item, QEvent *event);
+private:
+    QDeclarativeItem* m_start;
+    QDeclarativeItem* m_end;
+
+    void registerItem(QDeclarativeItem *start);
+    
+};
+
+#endif // EDGELAYER_H
