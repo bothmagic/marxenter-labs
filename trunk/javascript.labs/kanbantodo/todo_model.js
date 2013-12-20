64085a8b56efe8946676e335da6428dac99724b7
markus.taubert@googlemail.com <markus.taubert@googlemail.com@c8aec436-628a-29fb-4c76-4f2700b0ca23>
2013-12-20 10:36:53 +0000

    backup

--- trunk/javascript.labs/kanbantodo/todo_model.js
+++ trunk/javascript.labs/kanbantodo/todo_model.js
@@ -6,46 +6,32 @@
  * To change this template use File | Settings | File Templates.
  */
 
-YUI().use(
-    'model', function(Y) {
-// Create a new Y.PieModel class that extends Y.Model.
+YUI.add('todomodel', function (Y) {
 
-Y.PieModel = Y.Base.create('pieModel', Y.Model, [], {
-    // Add prototype methods for your Model here if desired. These methods will be
-    // available to all instances of your Model.
 
-    // Returns true if all the slices of the pie have been eaten.
-    allGone: function () {
-        return this.get('slices') === 0;
-    },
 
-    // Consumes a slice of pie, or fires an `error` event if there are no slices
-    // left.
-    eatSlice: function () {
-        if (this.allGone()) {
-            this.fire('error', {
-                type : 'eat',
-                error: "Oh snap! There isn't any pie left."
-            });
-        } else {
-            this.set('slices', this.get('slices') - 1);
-            Y.log('You just ate a slice of delicious ' + this.get('type') + ' pie!');
+    Y.TodoModel = Y.Base.create('todoModel', Y.Model, [Y.ModelSync.REST], {
+        root: 'http://symfony.localhost/web/app_dev.php/todos'
+
+    }, {
+        ATTRS: {
+
+            shortDesc: {
+                value: ''
+            },
+
+            longDesc: {
+                value: ''
+            }
         }
-    }
-}, {
-    ATTRS: {
-        // Add custom model attributes here. These attributes will contain your
-        // model's data. See the docs for Y.Attribute to learn more about defining
-        // attributes.
+    });
 
-        slices: {
-            value: 6 // default value
-        },
 
-        type: {
-            value: 'apple'
-        }
-    }
+    Y.TodoModels = Y.Base.create('users', Y.ModelList, [Y.ModelSync.REST], {
+
+        model: Y.TodoModel
+    });
+
+}, '0.0.1', {
+    requires: ['model', 'model-list', 'model-sync-rest']
 });
-    })
-
